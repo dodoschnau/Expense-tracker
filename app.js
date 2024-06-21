1fe5ba18f3962c8045555e363b571f32eafa0221
dodoschnau <huangmeisin@gmail.com>
2024-06-21 16:03:39 +0800

    build: add bootstrap static files for style

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.redirect('/expenses')
 })
