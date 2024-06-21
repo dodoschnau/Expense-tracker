1512cc59e8c0d4e94f8880bf86909166c41bfc12
dodoschnau <huangmeisin@gmail.com>
2024-06-21 14:04:44 +0800

    build: expenses routes

--- app.js
+++ app.js
@@ -6,6 +6,38 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
+app.get('/expenses', (req, res) => {
+  res.send('Get All Expenses.')
+})
+
+app.get('/expenses/new', (req, res) => {
+  res.send('Get New Expense Page.')
+})
+
+app.get('/expenses/:id', (req, res) => {
+  const id = req.params.id
+  res.send(`Get 'id: ${id}' Expense Page.`)
+})
+
+app.get('/expenses/:id/edit', (req, res) => {
+  const id = req.params.id
+  res.send(`Get Edit 'id: ${id}' Expense Page.`)
+})
+
+app.post('/expenses', (req, res) => {
+  res.send('Create A Expense Record.')
+})
+
+app.put('/expenses/:id', (req, res) => {
+  const id = req.params.id
+  res.send('Edit A Expense Record.')
+})
+
+app.delete('/expenses/:id', (req, res) => {
+  const id = req.params.id
+  res.send('Delete A Expense Record.')
+})
+
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
