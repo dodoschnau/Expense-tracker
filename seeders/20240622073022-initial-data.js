20a407740290943b3e8fe76ad8ff81138b2c2d25
dodoschnau <huangmeisin@gmail.com>
2024-06-23 21:57:37 +0800

    feat: seeding category data & make categoryId not null

--- seeders/20240622073022-initial-data.js
+++ seeders/20240622073022-initial-data.js
@@ -1,21 +1,44 @@
 'use strict';
 
 const expenses = require('../public/jsons/expenses.json')
+const categories = require('../public/jsons/categories.json')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Expenses', expenses.results.map(expense => ({
-      name: expense.name,
-      date: expense.date,
-      amount: expense.amount,
-      createdAt: new Date,
-      updatedAt: new Date()
-    })
-    ))
+
+    let transaction
+
+    try {
+
+      transaction = await queryInterface.sequelize.transaction()
+
+      await queryInterface.bulkInsert('Categories', categories.results.map(category => ({
+        name: category.name,
+        icon: category.icon,
+        createdAt: new Date,
+        updatedAt: new Date()
+      })), { transaction })
+
+      await queryInterface.bulkInsert('Expenses', expenses.results.map(expense => ({
+        name: expense.name,
+        date: expense.date,
+        amount: expense.amount,
+        categoryId: 3,
+        createdAt: new Date,
+        updatedAt: new Date()
+      })), { transaction })
+
+      await transaction.commit()
+
+    } catch (error) {
+      console.error('Error in seeding: ', error)
+      if (transaction) await transaction.rollback()
+    }
   },
 
   async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Categories', null)
     await queryInterface.bulkDelete('Expenses', null)
   }
 };
