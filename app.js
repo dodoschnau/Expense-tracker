d38ba435dc23d4919748892f76d65f176c8e8cf2
dodoschnau <huangmeisin@gmail.com>
2024-06-25 23:00:31 +0800

    feat: add success flash message by using message middleware

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 const methodOverride = require('method-override')
+const messageHandler = require('./middlewares/message-handler')
 
 const app = express()
 
@@ -33,6 +36,15 @@ app.set('views', './views')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+app.use(session({
+  secret: 'ThisIsSecret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
+app.use(messageHandler)
 
 app.get('/', (req, res) => {
   res.redirect('/expenses')
@@ -83,7 +95,6 @@ app.get('/expenses', (req, res) => {
         totalPages,
         sort
       })
-      console.log('rows = ', rows)
     })
     .catch((error) => console.log(error))
 })
@@ -126,7 +137,10 @@ app.get('/expenses/:id/edit', (req, res) => {
 app.post('/expenses', (req, res) => {
   const { name, date, amount, category } = req.body
   return Expense.create({ name, date, amount, categoryId: category })
-    .then(() => res.redirect('/expenses'))
+    .then(() => {
+      req.flash('success', 'Created successfully!')
+      res.redirect('/expenses')
+    })
     .catch((error) => console.log(error))
 })
 
@@ -135,7 +149,10 @@ app.put('/expenses/:id', (req, res) => {
   const id = req.params.id
   const { name, date, amount, category } = req.body
   return Expense.update({ name, date, amount, categoryId: category }, { where: { id } })
-    .then(() => res.redirect('/expenses'))
+    .then(() => {
+      req.flash('success', 'Updated successfully!')
+      res.redirect('/expenses')
+    })
     .catch((error) => console.log(error))
 })
 
@@ -143,7 +160,10 @@ app.put('/expenses/:id', (req, res) => {
 app.delete('/expenses/:id', (req, res) => {
   const id = req.params.id
   return Expense.destroy({ where: { id } })
-    .then(() => res.redirect('/expenses'))
+    .then(() => {
+      req.flash('success', 'Deleted successfully!')
+      res.redirect('/expenses')
+    })
     .catch((error) => console.log(error))
 })
 
